738a1b5dd36f34210c6e37d594554ed9c51cc271
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-21 15:15:01 -0300

    Criando primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express"
 
 const app = express()
 
+app.get("/test", (req, res) => {
+    return res.send("Hello World")
+})
+
+app.post("/test-post", (req, res) => {
+    return res.send("Olá NLW método POST")
+})
+
 app.listen(3000, () => console.log("Server is Running NLW"))
