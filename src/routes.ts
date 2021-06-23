edd8b78a6262c493fd06b7a4a66aa517baca9e31
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-23 15:10:52 -0300

    Tags conexão com banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import {Router} from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router();
 
+const createTagController = new CreateTagController();
 const createUserController = new CreateUserController();
 
 router.post("/users", createUserController.handle)
+router.post("/tags", createTagController.handle)
 
 export {router}
