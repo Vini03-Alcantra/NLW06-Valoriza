d19f1fca13a9f5b986e4587d0563f8a064dbcff2
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-25 16:13:05 -0300

    Obtendo id do user que está enviando pelo token

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -4,13 +4,14 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 class CreateComplimentController{
     async handle(req: Request, res: Response){
         const { tag_id, user_receiver, user_sender, message } = req.body;
-        
+        const {user_id} = req;
+
         const createComplimnetService = new CreateComplimentService()
 
         const compliment = await createComplimnetService.execute({
             tag_id, 
             user_receiver, 
-            user_sender, 
+            user_sender: user_id, 
             message
         })
 
